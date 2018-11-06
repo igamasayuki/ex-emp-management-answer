bc6aa78d68bed72e5d564da71ef5f6d5abc793b1
Masayuki IGA <igamasayuki@gmail.com>
2018-11-06 16:04:46 +0900

    従業員管理システム解答例完成

---
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -0,0 +1,98 @@
+package jp.co.sample.emp_management.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.emp_management.domain.Administrator;
+import jp.co.sample.emp_management.form.LoginForm;
+import jp.co.sample.emp_management.service.AdministratorService;
+
+/**
+ * 管理者情報を操作するコントローラー.
+ * 
+ * @author igamasayuki
+ *
+ */
+@Controller
+@RequestMapping("/administrator")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorService;
+
+	/**
+	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
+	 * 
+	 * @return フォーム
+	 */
+	@ModelAttribute
+	public LoginForm setUpForm() {
+		return new LoginForm();
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：管理者を登録する
+	/////////////////////////////////////////////////////
+	/**
+	 * 管理者登録画面を出力します.
+	 * 
+	 * @return 管理者登録画面
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
+
+	/**
+	 * 管理者情報を登録します.
+	 * 
+	 * @param form
+	 *            管理者情報用フォーム
+	 * @return ログイン画面へリダイレクト
+	 */
+	@RequestMapping("/insert")
+	public String insert(LoginForm form) {
+		Administrator administrator = new Administrator();
+		// フォームからドメインにプロパティ値をコピー
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/administrator/login";
+	}
+
+	/////////////////////////////////////////////////////
+	// ユースケース：ログインをする
+	/////////////////////////////////////////////////////
+	/**
+	 * ログイン画面を出力します.
+	 * 
+	 * @return ログイン画面
+	 */
+	@RequestMapping("/toLogin")
+	public String toLogin() {
+		return "administrator/login";
+	}
+
+	/**
+	 * ログインします.
+	 * 
+	 * @param form
+	 *            管理者情報用フォーム
+	 * @param result
+	 *            エラー情報格納用オブッジェクト
+	 * @return ログイン後の従業員一覧画面
+	 */
+	@RequestMapping("/login")
+	public String login(LoginForm form, BindingResult result) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if (administrator == null) {
+			result.addError(new ObjectError("loginError", "メールアドレスまたはパスワードが不正です。"));
+			return toLogin();
+		}
+		return "forward:/employee/showList";
+	}
+}
