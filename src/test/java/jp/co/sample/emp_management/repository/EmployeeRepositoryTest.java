7490f9743367bd5503e4aede4beead25b6660a98
igamasayuki <igamasayuki@gmail.com>
2021-05-27 16:26:49 +0900

    SpringBootのバージョンを2.5に上げました

--- src/test/java/jp/co/sample/emp_management/repository/EmployeeRepositoryTest.java
+++ src/test/java/jp/co/sample/emp_management/repository/EmployeeRepositoryTest.java
@@ -4,15 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import jp.co.sample.emp_management.domain.Employee;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeeRepositoryTest {
 
