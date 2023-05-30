5fd86df416046987958e683ffbcdd23c76753af1
igamasayuki <igamasayuki@gmail.com>
2023-05-30 09:34:49 +0900

    Mockitoを使用しているのでその解説コメントを追加しました

--- src/test/java/com/example/service/AdministratorServiceTest.java
+++ src/test/java/com/example/service/AdministratorServiceTest.java
@@ -1,17 +1,25 @@
 package com.example.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
+/**
+ * Mockito(モキート)を使用したサービスクラスのテストクラス.<br>
+ * 
+ * リポジトリクラスが未実装でもMockitoを使用すればサービスのテストができるのでそのサンプルです。
+ * 
+ * @author igamasayuki
+ *
+ */
 @SpringBootTest
 class AdministratorServiceTest {
 
@@ -20,11 +28,14 @@ class AdministratorServiceTest {
 	private AdministratorService administratorService;
 
 	// テスト対象クラス内で使われている未実装のクラス
+	// このクラスを実行する際にAdministratorRepositoryクラスの各メソッド内を全てコメントアウトして未実装状態にしておきます。
+	// 戻り値を返すメソッドなら「return null;」を書いておく
 	@Mock
 	private AdministratorRepository administratorRepository;
 
 	@Test
 	void testLogin() {
+		// 未実装のAdministratorRepository内のfindByMailAddressAndPassward()の動きを定義する
 		when(administratorRepository.findByMailAddressAndPassward("iga@sample.com", "igaigaiga"))
 				.thenReturn(new Administrator(1, "伊賀", "iga@sample.com", "igaigaiga"));
 
@@ -42,17 +53,17 @@ class AdministratorServiceTest {
 		Administrator administrator = new Administrator(1, "伊賀", "iga@sample.com", "igaigaiga");
 		administratorService.insert(administrator);
 
-		// 以下はadministratorRepositoryのinsert(administrator)が実行されたかどうかテストしている。
-		Mockito.verify(administratorRepository).insert(administrator);
+		// administratorRepositoryのinsert(administrator)が呼ばれたかどうかテストしている。
+		verify(administratorRepository).insert(administrator);
 	}
 
 	@Test
 	void testLogin2() {
 		administratorService.login("iga@sample.com", "igaigaiga");
 
-		// 以下はadministratorRepositoryのfindByMailAddressAndPassward("iga@sample.com",
-		// "igaigaiga")が実行されたかどうかテストしている。
-		Mockito.verify(administratorRepository).findByMailAddressAndPassward("iga@sample.com", "igaigaiga");
+		// 以下はadministratorRepositoryの
+		// findByMailAddressAndPassward("iga@sample.com","igaigaiga")が実行されたかどうかテストしている。
+		verify(administratorRepository).findByMailAddressAndPassward("iga@sample.com", "igaigaiga");
 	}
 
 }
